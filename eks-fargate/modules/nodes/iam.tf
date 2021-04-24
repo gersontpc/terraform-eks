3e014f3cf6497dd70d69c78cd55eafac65666a57
Gerson Carneiro <gerson.carneiro@evoluservices.com>
2021-04-24 00:36:29 -0300

    IAM StsAssumeRole

--- eks-fargate/modules/nodes/iam.tf
+++ eks-fargate/modules/nodes/iam.tf
@@ -1,3 +1,22 @@
+data "aws_iam_policy_document" "eks_nodes_role" {
+
+    version = "2012-10-17"
+
+    statement {
+
+        actions = [ "sts:AssumeRole" ]
+
+        principals {
+            type = "Service"
+            identifiers = [
+                "ec2.amazonaws.com"
+            ]
+        }
+
+    }
+
+}
+
 resource "aws_iam_role" "fargate_role" {
   name = "eks-fargate-profile"
 
